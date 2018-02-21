07eed88a280059864681bb53f1416c109a100a6c
Federico Krum <federico@krum.com.ar>
2018-02-21 19:23:58 +0100

    Update build.bash

--- build.bash
+++ build.bash
@@ -5,4 +5,6 @@ apt install transmission-cli transmission-common transmission-daemon
 
 # ---------- Configurar Transmission ----------
 service transmission-daemon stop
-# 
+#sudo nano /var/lib/transmission-daemon/info/settings.json
+
+service transmission-daemon stop
